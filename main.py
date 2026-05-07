10b9eaf09d0cd6ac96b9ea0e712d8b22874e2ed4
kaya-ilker <ilkeraya+github@gmail.com>
2026-05-08 00:32:32 +0300

    Ilk moduler surum ve GitHub hazirligi

---
+++ main.py
@@ -0,0 +1,5 @@
+import customtkinter as ctk
+import threading
+from downloader import download_engine # Kendi yazdığımız dosyadan çağırıyoruz
+from converter import convert_engine     # Kendi yazdığımız dosyadan çağırıyoruz
+# ... (v3.4 arayüz kodlarının geri kalanı buraya gelecek)
